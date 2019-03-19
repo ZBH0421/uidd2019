348982d7821fb7ddc28ce7f77a1a4b9bc244d056
yolo6042 <firetruck6421@gmail.com>
2019-03-19 15:26:58 +0800

    second

--- week3.js
+++ week3.js
@@ -1,44 +1,197 @@
-//image =dr2 empties=or2
+var arr=[];
+
+       $( function() {
+
+         console.log('hey');
+         $( "#df1" ).draggable({
+          
+           revert:"invalid"
+             
+         });
+
+         $( "#or1" ).droppable({
+
+           accept:'#df1',
+  
+           drop: function( event, ui ) {
+             
+             $(this).css("opacity","1")
+             $("or1").append (ui.draggable);
+             $(ui.draggable).css({position:"relative",top:"0px",left:"0px"})
+              
+             .draggable("disable")
+             .css({opacity:0});
+             arr='b';
+             /*
+             $( this )
+
+               .addClass( "ui-state-highlight" )
+
+               .find( "p" )
+
+               .html( "Dropped!" );
+
+               
+  */        
+           }
+
+         });
+
+console.log('dropdetect');
+       } );
+
+    
+       $( function() {
+
+         console.log('hey');
+         $( "#df2" ).draggable({
+          
+           revert:"invalid"
+             
+         });
+
+         $( "#or2" ).droppable({
+
+           accept:'#df2',
+  
+           drop: function( event, ui ) {
+             $(this).css("opacity","1")
+             $("or2").append (ui.draggable);
+             $(ui.draggable).css({position:"relative",top:"0px",left:"0px"})
+              
+             .draggable("disable")
+             .css({opacity:0});
+             /*
+             $( this )
+
+               .addClass( "ui-state-highlight" )
+
+               .find( "p" )
+
+               .html( "Dropped!" );
+
+               
+  */        
+           }
+
+         });
+
+console.log('dropdetect');
+       } );
 
 
+       $( function() {
 
+         console.log('hey');
+         $( "#df3" ).draggable({
+          
+           revert:"invalid"
+             
+         });
 
+         $( "#or3" ).droppable({
+
+  
+           accept:'#df3',
+           drop: function( event, ui ) {
+             $(this).css("opacity","1")
+             $("or3").append (ui.draggable);
+             $(ui.draggable).css({position:"relative",top:"0px",left:"0px"})
+              
+             .draggable("disable")
+             .css({opacity:0});
+             /*
+             $( this )
+
+               .addClass( "ui-state-highlight" )
+
+               .find( "p" )
+
+               .html( "Dropped!" );
+
+               
+  */        
+           }
+
+         });
+
+console.log('dropdetect');
+       } );
+
+       $( function() {
+
+         console.log('hey');
+         $( "#df4" ).draggable({
+          
+           revert:"invalid"
+             
+         });
+
+         $( "#or4" ).droppable({
+
+           accept:'#df4',
+  
+           drop: function( event, ui ) 
+           {
+             $(this).css("opacity","1")
+             $("or4").append (ui.draggable);
+             $(ui.draggable).css({position:"relative",top:"0px",left:"0px"})
+              
+             .draggable("disable")
+             .css({opacity:0});
+             /*
+             $( this )
+
+               .addClass( "ui-state-highlight" )
+
+               .find( "p" )
+
+               .html( "Dropped!" );
+
+               
+  */        
+           }
+
+         });
+
+console.log('dropdetect');
+       } );
 /*
- *
- *
-var id;
 
-let dragSource = document.querySelector('#df1')
-dragSource.addEventListener('dragstary',dragStart)
-
-let dropTarget = document.querySelector('#or1')
+let sourceContainerId ="";
+let dragSources = document.querySelectorAll('[draggable="true"]')
+dragSources.forEach(dragSource =>{
+  dragSource.addEventListener('dragstart',dragStart);
+  dragSource.addEventListener("dragged",dragEnd);  
+})
+let dropTargets = document.querySelectorAll('[data-role="drag-drop-container"]')
+dropTargets.forEach(dropTarget =>{
 dropTarget.addEventListener('drop',dropped)
 dropTarget.addEventListener('dragenter',cancelDefault)
 dropTarget.addEventListener('dragover',cancelDefault)
+})
 
 function cancelDefault (e){
-
 e.preventDefault()
 e.stopPropagation()
 return false
 }
-function dragStart1(e){
 
-console.log('dragStart')
-  e.dataTransfer.serData('text/plain',e.target.id)
-
+function dragStart(e){
+  e.dataTransfer.setData('text/plain',e.target.id);
+  sourceContainerId=this.parentElement.id;
+  console.log('sourceContainerId',sourceContainerId);
+  
 }
 function dropped(e){
-console.log('dropped')
+  if (this.id !==sourceContainerId){
+  console.log('dropped')
   cancelDefault(e)
-  let id=e.dataTransfer.getDate('text/plain')
-  e.target.appenChild(document.querySelector('#' + id))
+  let id = e.dataTransfer.getData('text/plain')
+  e.target.appendChild(document.querySelector('#' + id))
 }
-
-
-
-
-
+}
+/*
 const dr2=document.querySelector('.df2');
 const or2=document.querySelectorAll('.or2');
 
@@ -80,7 +233,6 @@ function dragDrop(){
   this.append(df2);
 console.log('drop');
 }
+*/
 
 
- *
- * */
